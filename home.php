bc1f24892303dd493341895528e9ba6606ca7f07
Muhammed <muhammedkocak.dev@gmail.com>
2019-05-10 01:27:02 +0200

    for loop

--- home.php
+++ home.php
@@ -29,5 +29,28 @@ var $engine = "454";
 $chassis = new crewCab;
 
 
-
+//2Write a for-loop that adds an item to all of the above.
+for($i = 0; $i <= 3; $i++){
+  if($i === 1){
+  array_push($chevy,'AVALANCHE');
+  }
+  else if($i === 2){
+  array_push($favModels["BLAZER"], 'S-10');
+  array_push($favModels["SUBURBAN"], 'SCOTTSDALE', 'SILVERADO');
+  array_push($favModels["C/K30"], '', 'UTILITYCAB');
+  
+  }
+  else if($i === 3){
+  $chassis->color = 'black';
+  }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
 ?>
